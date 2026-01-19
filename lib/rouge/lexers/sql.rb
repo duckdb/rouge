e795f2d9c40d56422c4a3f71752706be634bef87
Gabor Szarnyas <szarnyasg@gmail.com>
2026-01-19 13:06:28 +0100

    option

--- lib/rouge/lexers/sql.rb
+++ lib/rouge/lexers/sql.rb
@@ -2242,6 +2242,7 @@ module Rouge
           force_download
           functions
           hnsw_ef_search
+          hnsw_enable_experimental_persistence
           home_directory
           http_keep_alive
           http_logging_output
