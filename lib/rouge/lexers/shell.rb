fde3568afaecb7c996ee848d51edce2e5518142a
Gabor Szarnyas <szarnyasg@gmail.com>
2026-02-24 14:06:45 +0100

    add cli command

--- lib/rouge/lexers/shell.rb
+++ lib/rouge/lexers/shell.rb
@@ -49,6 +49,7 @@ module Rouge
         isql clone hexdump iconv pacman gzcat flutter zstd unzstd zstdmt
         pkg clang sh ksh fish bash zsh marimo psql sqlite sqlite3 uv uvx
         lsattr chattr ldd otool yay docker tpchgen-cli xcode-select lldb gdb
+        composer
       ).join('|')
 
       state :basic do
