6c2a4455cd0d2ec349f0515eed34c2a08b4732c2
Gabor Szarnyas <szarnyasg@gmail.com>
2026-03-25 15:57:26 +0100

    Add PS

--- lib/rouge/lexers/batchfile.rb
+++ lib/rouge/lexers/batchfile.rb
@@ -42,7 +42,7 @@ module Rouge
         isql clone hexdump iconv pacman gzcat flutter zstd unzstd zstdmt
         pkg clang sh ksh fish bash zsh marimo psql sqlite sqlite3 uv uvx
         lsattr chattr ldd otool yay docker tpchgen-cli xcode-select lldb gdb
-        composer
+        composer powershell
       ).join('|')
 
       state :basic do
